090cc6a36c207b56ab21847e1f44a13b54307981
test <[nkaneko00@icloud.com]>
2020-05-11 00:39:09 +0900

    kadai5

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,25 @@
 class FavoritesController < ApplicationController
 
 	#index,newアクションは作成必要ない
-	def create
-		book = Book.find(params[:book_id])
-		favorite = current_user.favorites.new(book_id: book.id)
-		favorite.save
-		# リダイレクト先を同じページにする(更新のような形)
-		redirect_back(fallback_location: root_path)
-	end
-	def destroy
-		book = Book.find(params[:book_id])
-		favorite = current_user.favorites.find_by(book_id: book.id)
-		favorite.destroy
-		# リダイレクト先を同じページにする(更新のような形)
-		redirect_back(fallback_location: root_path)
-	end
+	  def create
+	    current_user.follow(params[:user_id])
+	    # リダイレクト先を同じページにする(更新のような形)
+	    redirect_to request.referer
+	  end
+	  
+	  def destroy
+	    current_user.unfollow(params[:user_id])
+	    # リダイレクト先を同じページにする(更新のような形)
+	    redirect_back(fallback_location: root_path)
+	  end
 
+	  def follower
+	    user = User.find(params[:user_id])
+	    @users = user.following_user
+	  end
+
+	  def followed
+	    user = User.find(params[:user_id])
+	    @users = user.follower_user
+	  end
 end
